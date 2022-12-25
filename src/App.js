0f5fdcffa68d13564e760e650577ae6f6e5c5b4d
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2022-12-25 11:42:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import toast, { Toaster } from 'react-hot-toast';
 import React, { Component } from 'react';
 import PokemonForm from './components/Pokemon/PokemonForm';
 import PokemonInfo from './components/Pokemon/PokemonInfo';
-// import { CountryApp } from './components/Country/CountryApp';
+import { CountryApp } from './components/Country/CountryApp';
 
 export default class App extends Component {
   state = {
@@ -15,27 +15,17 @@ export default class App extends Component {
 
   handleFormSubmit = pokemonName => {
     this.setState({ pokemonName });
-    // console.log(pokemonName);
   };
-  // componentDidMount() {
-  //   this.setState({ loading: true });
-  //   setTimeout(() => {
-  //     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  //       .then(res => res.json())
-  //       .then(pokemon => this.setState({ pokemon }))
-  //       .finally(() => this.setState({ loading: false }));
-  //   }, 1000);
-  // }
 
   render() {
     return (
       <div>
-        <PokemonForm onSubmitProp={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
+        {/* <PokemonForm onSubmitProp={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} /> */}
         <Toaster />
-        {/* <CountryApp /> */}
-        {/* {this.state.loading && <h1>Loading</h1>}
-        {this.state.pokemon && <div>{this.state.pokemon.name}</div>} */}
+        <CountryApp />
+        {this.state.loading && <h1>Loading</h1>}
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
       </div>
     );
   }
