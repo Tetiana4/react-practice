48fcfb0e1d55ad26887461b207f41a486d8c2716
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-01-04 18:39:09 +0100

    Change name

---
+++ src/components/Country/CountryForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Form, Input, SearchButton } from './Country.styled';
+
+export const CountryForm = ({ onSubmit }) => {
+  return (
+    <Form
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(e.target.elements.countryName.value);
+      }}
+      autoComplete="off"
+    >
+      <Input type="text" name="countryName" />
+      <SearchButton type="submit">Search</SearchButton>
+    </Form>
+  );
+};
