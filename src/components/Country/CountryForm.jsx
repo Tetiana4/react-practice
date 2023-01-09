c32d84abc8f7ecc7f0f36468c98f583eaa5a361f
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-01-09 13:03:57 +0100

    Update CountryForm.jsx

--- src/components/Country/CountryForm.jsx
+++ src/components/Country/CountryForm.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 import { Form, Input, SearchButton } from './Country.styled';
 
 export const CountryForm = ({ onSubmit }) => {
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    const query = e.target.elements.countryName.value;
+    if (query.trim() === '') {
+      return toast.error('Please, write country name');
+    }
+    onSubmit(query);
+  };
+
   return (
-    <Form
-      onSubmit={e => {
-        e.preventDefault();
-        onSubmit(e.target.elements.countryName.value);
-      }}
-      autoComplete="off"
-    >
-      <Input type="text" name="countryName" placeholder="search country" />
+    <Form onSubmit={handleFormSubmit}>
+      <Input
+        type="text"
+        name="countryName"
+        placeholder="search country"
+        autoComplete="off"
+      />
       <SearchButton type="submit">Search</SearchButton>
     </Form>
   );
