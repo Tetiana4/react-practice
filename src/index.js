6e23228cd1e71384873127ace3fec35df240fc95
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-01-10 14:06:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { DataProvider } from './components/Form/FormContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DataProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
