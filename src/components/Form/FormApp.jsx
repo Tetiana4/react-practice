3104104482aa2404a463eea27251c7e1f61b7d72
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-01-16 17:26:31 +0100

    Update code

--- src/components/Form/FormApp.jsx
+++ src/components/Form/FormApp.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 // import { Routes, Route, Outlet } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
 import { Header } from './Header';
 import { StepOne } from './StepOne';
-
+const navItem = [{ href: 'step2' }, { href: 'step3' }, { href: 'result' }];
 export const FormApp = () => {
   return (
     <>
       <Header />
+      <ul>
+        {navItem.map(({ href }) => (
+          <NavLink key={href} to={href}></NavLink>
+        ))}
+      </ul>
       <StepOne />
       <Outlet />
     </>
--- src/components/Form/StepOne.jsx
+++ src/components/Form/StepOne.jsx
@@ -2,11 +2,16 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Input } from './Input';
+// import { Header } from './Header';
+
 import { PrimaryButton } from './PrimaryButton';
 import { MainContainer } from './MainContainer';
 import { Form } from './Form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
+
 const schema = yup.object().shape({
   firstName: yup
     .string()
@@ -18,37 +23,46 @@ const schema = yup.object().shape({
     .required('Last name is a required field'),
 });
 export const StepOne = () => {
-  const { register, handleSubmit, errors } = useForm({
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     mode: 'onBlur',
     resolver: yupResolver(schema),
   });
-  const onSubmit = data => console.log(data);
+
+  const onSubmit = data => {
+    navigate('step2');
+  };
+
   return (
-    <MainContainer>
-      <Typography component="h2" variant="h5">
-        {/* StepOne */}
-      </Typography>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          {...register('firstName')}
-          id="firstName"
-          type="text"
-          label="First Name"
-          name="firstName"
-          error={!!errors.firstName}
-          helperText={errors?.firstName?.message}
-        />
-        <Input
-          {...register('lastName')}
-          id="lastName"
-          type="text"
-          label="Last Name"
-          name="lastName"
-          error={!!errors.lastName}
-          helperText={errors?.lastName?.message}
-        />
-        <PrimaryButton>Next</PrimaryButton>
-      </Form>
-    </MainContainer>
+    <>
+      <MainContainer>
+        <Typography component="h2" variant="h5"></Typography>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register('firstName')}
+            id="firstName"
+            type="text"
+            label="First Name"
+            name="firstName"
+            error={!!errors.firstName}
+            helperText={errors.firstName && `${errors.firstName.message}`}
+          />
+          <Input
+            {...register('lastName')}
+            id="lastName"
+            type="text"
+            label="Last Name"
+            name="lastName"
+            error={!!errors.lastName}
+            helperText={errors.lastName && `${errors.lastName.message}`}
+          />
+          <PrimaryButton>Next</PrimaryButton>
+        </Form>
+      </MainContainer>
+    </>
   );
 };
