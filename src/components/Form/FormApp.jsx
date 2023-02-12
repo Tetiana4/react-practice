be8ebf283894b2cc5646157b09f54ee031d9bf6f
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2023-02-12 19:28:20 +0100

    Update FormApp.jsx

--- src/components/Form/FormApp.jsx
+++ src/components/Form/FormApp.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
-import { StepOne } from './StepOne';
 
 export const FormApp = () => {
   return (
     <>
       <Header />
-      <StepOne />
       <Outlet />
     </>
   );
